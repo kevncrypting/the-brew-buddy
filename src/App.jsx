0599d0af23a31b229eaf4fb13c94bfb2a6a5cf11
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-23 12:16:02 -0400

    Adds info to README, also begins work on react router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import Bio from "./Bio.jsx";
+import Book from "./Book.jsx";
+import Buddy from "./Buddy.jsx";
+import NoMatch from "./NoMatch.jsx";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">TTThis is the beginning of my React front-end project!</h1>
+      <header className="flex gap-1">
+        <h1>the brew </h1>
+        <nav className="flex gap-1">
+          <NavLink
+            className={({ isActive }) => (isActive ? activeLink : undefined)}
+            to="/book"
+          >
+            book
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? activeLink : undefined)}
+            to="/buddy"
+          >
+            buddy
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? activeLink : undefined)}
+            to="/bio"
+          >
+            bio
+          </NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/book" element={<Book />} />
+        <Route path="/buddy" element={<Buddy />} />
+        <Route path="/bio" element={<Bio />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Bio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bio = () => {
+  return (
+    <>
+      <Link to={`/bio`}>Bio</Link>
+    </>
+  );
+};
+
+export default Bio;
