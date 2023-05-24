61109da13ef1d1d63d8649b9e1f2f16cd884a668
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-24 06:48:43 -0400

    Tests fetching of Pexels API data successfully

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 const Book = () => {
-  return (
-    <div>Book</div>
-  )
-}
+  const [imageGallery, setImageGallery] = useState([]);
 
-export default Book
+  useEffect(() => {
+    fetch(`https://api.pexels.com/v1/search?query=coffee&per_page=15`, {
+      headers: {
+        Authorization: `${process.env.PEXELS_API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setImageGallery(data.photos);
+      });
+  });
+
+  const changeImage = (event) => {
+    event.preventDefault();
+    console.log(imageGallery);
+
+  }
+
+  return (
+    <>
+      <div>Book</div>
+      <button onClick={changeImage}>Refresh Image</button>
+      <a href="https://www.pexels.com">Photos provided by Pexels</a>
+    </>
+  );
+};
+
+export default Book;
