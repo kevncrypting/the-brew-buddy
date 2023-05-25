b2addd1bbc24350468d703b177bc985d0c4e5f8a
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-25 13:57:09 -0400

    Finishes card styling on book

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,17 @@
-const Card = ({ title, thoughts, id }) => {
+const Card = ({ title, thoughts, url }) => {
   return (
-    <div className="bg-brown-400 rounded p-6 my-2">
-        
+    <div className="flex bg-brown-400 rounded p-6 my-2 gap-3">
+      <img
+        src={`https://cataas.com/${url}`}
+        alt="A cat picture"
+        className="object-cover h-48 w-48 rounded-xl"
+      />
+      <div>
         <h3 className="text-xl font-bold">{title}</h3>
         <p>{thoughts}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
